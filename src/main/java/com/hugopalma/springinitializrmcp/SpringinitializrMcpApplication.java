edb4583e140018ce244d859a69acd118ce6ae8db
Hugo Palma <hugo.m.palma@gmail.com>
2025-05-30 23:02:04 +0100

    template files

---
+++ src/main/java/com/hugopalma/springinitializrmcp/SpringinitializrMcpApplication.java
@@ -0,0 +1,13 @@
+package com.hugopalma.springinitializrmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinitializrMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinitializrMcpApplication.class, args);
+	}
+
+}
